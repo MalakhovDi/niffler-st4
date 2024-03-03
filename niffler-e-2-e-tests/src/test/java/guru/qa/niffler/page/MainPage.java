b1b5fc28710f7dd53cd0d06ada73eb195a23c0f1
malahovda <psod93@mail.ru>
2024-03-03 17:32:27 +0300

    HomeWork_12

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/MainPage.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/MainPage.java
@@ -1,34 +1,38 @@
 package guru.qa.niffler.page;
 
+import guru.qa.niffler.model.SpendJson;
 import guru.qa.niffler.page.message.SuccessMsg;
 import guru.qa.niffler.page_element.HeaderMainPageElement;
 import guru.qa.niffler.page_element.HistoryOfSpendingsElement;
 import guru.qa.niffler.page_element.StatsElement;
 import io.qameta.allure.Step;
 
+import java.io.IOException;
+import java.util.List;
+
 public class MainPage extends BasePage<MainPage> {
 
-    private final HistoryOfSpendingsElement HistoryOfSpendingsElement = new HistoryOfSpendingsElement();
+    private final HistoryOfSpendingsElement historyOfSpendingsElement = new HistoryOfSpendingsElement();
     private final HeaderMainPageElement headerMainPageElement = new HeaderMainPageElement();
     private final StatsElement statsElement = new StatsElement();
 
     @Override
     @Step("Ожидание загрузки главной страницы")
     public MainPage waitUntilLoaded() {
-        HistoryOfSpendingsElement.waitUntilLoaded();
+        historyOfSpendingsElement.waitUntilLoaded();
         return this;
     }
 
     @Step("Удалить Spending по названию {spendDescription}")
     public MainPage deleteSpendingByButtonDelete(String spendDescription) {
-        HistoryOfSpendingsElement.deleteSpendingByButtonDeleteSpending(spendDescription);
+        historyOfSpendingsElement.deleteSpendingByButtonDeleteSpending(spendDescription);
         checkToastMessage(SuccessMsg.SPENDINGS_DELETED);
         return this;
     }
 
     @Step("Проверить пустой список Spendings")
     public MainPage verifyEmptyListOfSpendings() {
-        HistoryOfSpendingsElement.verifyEmptyListOfSpendings();
+        historyOfSpendingsElement.verifyEmptyListOfSpendings();
         return this;
     }
 
@@ -53,6 +57,37 @@ public class MainPage extends BasePage<MainPage> {
     @Step("Проверка наличия раздела Статистика")
     public MainPage verifyStatisticsSectionShouldExist() {
         statsElement.verifyStatisticsSectionShouldExist();
+        return this;
+    }
+
+    @Step("Проверить все spendings пользователя {username}")
+    public MainPage checkAllSpends(String username) throws IOException {
+        List<SpendJson> spends = spendApiClient.getSpends(username);
+        historyOfSpendingsElement.checkSpends(spends.toArray(new SpendJson[spends.size()]));
+        return this;
+    }
+
+    @Step("Выделить spend по индексу {index}")
+    public MainPage selectSpendingByIndex(int index) {
+        historyOfSpendingsElement.selectSpendingByIndex(index);
+        return this;
+    }
+
+    @Step("Снять выделение spend по индексу {index}")
+    public MainPage unSelectSpendingByIndex(int index) {
+        historyOfSpendingsElement.unSelectSpendingByIndex(index);
+        return this;
+    }
+
+    @Step("Выделить spend по описанию {description}")
+    public MainPage selectSpendingByDescription(String description) {
+        historyOfSpendingsElement.selectSpendingByDescription(description);
+        return this;
+    }
+
+    @Step("Снять выделение spend по описанию {description}")
+    public MainPage unSelectSpendingByDescription(String description) {
+        historyOfSpendingsElement.selectSpendingByDescription(description);
         return this;
     }
 }
--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page_element/HistoryOfSpendingsElement.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page_element/HistoryOfSpendingsElement.java
@@ -1,14 +1,21 @@
 package guru.qa.niffler.page_element;
 
 import com.codeborne.selenide.SelenideElement;
+import guru.qa.niffler.model.SpendJson;
+import guru.qa.niffler.page.component.BaseComponent;
 
 import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
+import static guru.qa.niffler.condition.SpendCollectionCondition.spends;
 
-public class HistoryOfSpendingsElement {
+public class HistoryOfSpendingsElement extends BaseComponent<HistoryOfSpendingsElement> {
+
+    public HistoryOfSpendingsElement() {
+        super($("section[class*='history']"));
+    }
     private final SelenideElement
             title = $(withText("History of spendings")),
             subtitle = $(withText("All the limits are in your head")),
@@ -35,6 +42,33 @@ public class HistoryOfSpendingsElement {
     public HistoryOfSpendingsElement verifyEmptyListOfSpendings() {
         tableOfSpendings.$$("tr")
                 .shouldHave(size(0));
+        return this;
+    }
+
+    public HistoryOfSpendingsElement checkSpends(SpendJson... expectedSpends) {
+        tableOfSpendings.$$("tr").shouldHave(spends(expectedSpends));
+        return this;
+    }
+
+    public HistoryOfSpendingsElement selectSpendingByIndex(int index) {
+        tableOfSpendings.$$("tr").get(index).$("[type='checkbox']").scrollTo().click();
+        buttonDeleteSelectedSpending.shouldNotHave(attribute("disabled"));
+        return this;
+    }
+
+    public HistoryOfSpendingsElement unSelectSpendingByIndex(int index) {
+        tableOfSpendings.$$("tr").get(index).$("[type='checkbox']").scrollTo().click();
+        return this;
+    }
+
+    public HistoryOfSpendingsElement selectSpendingByDescription(String description) {
+        tableOfSpendings.$$("tr").find(text(description)).$("[type='checkbox']").scrollTo().click();
+        buttonDeleteSelectedSpending.shouldNotHave(attribute("disabled"));
+        return this;
+    }
+
+    public HistoryOfSpendingsElement unSelectSpendingByDescription(String description) {
+        tableOfSpendings.$$("tr").find(text(description)).$("[type='checkbox']").scrollTo().click();
         return this;
     }
 }
