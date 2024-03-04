8cdc64134e8fa04b9e76a15f909042faef9cc9f5
malahovda <psod93@mail.ru>
2024-03-04 18:35:50 +0300

    tech_commit

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/message/SuccessMsg.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/message/SuccessMsg.java
@@ -5,7 +5,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum SuccessMsg implements Msg {
     PROFILE_SUCCESSFULLY_UPDATED("Profile successfully updated"),
-    SPENDINGS_DELETED("Spendings deleted");
+    SPENDINGS_DELETED("Spendings deleted"),
+    FRIEND_DELETED("Friend is deleted"),
+    INVITATION_ACCEPTED("Invitation is accepted"),
+    INVITATION_DECLINED("Invitation is declined"),
+    SPENDING_ADDED("Spending successfully added");
 
     private final String msg;
 
